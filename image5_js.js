cb943c70f45d7843c8f68e36586707024ec5e6e1
narendravaliveti <narendra.valiveti9977@gmail.com>
2018-11-08 15:27:50 +0530

    initial project commit

---
+++ image5_js.js
@@ -0,0 +1,75 @@
+	var tim,
+		tim1,
+		tim2;
+	    a = 0;
+
+	for(let i=1;i<=45;i++){
+		$("#img_cvr").append("<div>"+i+"</div>");
+	}
+
+	    $("#img_cvr div").click ( function () {
+			a++;
+			if (a<4) {
+				this.style.background="transparent";
+				this.innerHTML="";					
+					game_timer(a);
+			}
+	    });		
+
+	function game_timer(b) {
+		let t=0,
+		    stp = 0;
+		if(b==1){
+			t=45;
+			stp = 1;
+			tim = setInterval(timer,1000);
+		}else if(b==2){				
+			t=30;
+			stp = 2;
+			clearInterval(tim);
+			tim1 = setInterval(timer,1000);
+		}else if(b==3){
+	        t=15;
+	        stp = 3;
+	        clearInterval(tim1);
+	        tim2 = setInterval(timer,1000);
+		}
+		function timer() {
+	        --t;	            
+	            $("#l_cnt").html("Timer"+stp);        
+	            $("#timer").html("");
+	            $("#timer").html(t);
+	        if( t == 0 ) {
+	            alert("timeout");
+	            clearInterval(tim);
+	            clearInterval(tim1);
+	            clearInterval(tim2);
+	            $("#img_cvr").hide();	             	             
+	        }             	
+	    }	    		    	
+	}
+
+	function srch(){
+		let txt = $("#inp :nth-child(2)").val(),
+		    txt_ary = ["NTR","N T R"],
+		    t=0;
+		if (txt != "") {
+			let a = txt.toUpperCase();
+			for (let i = 0; i < txt_ary.length; i++) {
+				if(a === txt_ary[i]){
+					t++;
+				}
+			}
+			if (t===1){
+				alert("correct");
+				clearInterval(tim);
+	            clearInterval(tim1);
+	            clearInterval(tim2);
+				$("#img_cvr").hide();
+			}else {
+					alert("incorrect");
+				}
+		}else{
+			alert("enter input");
+		}
+	}
---
+++ imgGame_js.js
@@ -0,0 +1,3 @@
+for(i=1;i<=9;i++){
+	$("#menu").append("<div>"+"<a href="+"image"+i+".html"+">"+i+"</a>"+"</div>");
+}
